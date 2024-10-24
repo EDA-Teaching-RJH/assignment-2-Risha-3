d24098dc4214064481a1b27f0ab3ed75aa5e90ad
Risha-3 <rs989@kent.ac.uk>
2024-10-24 20:53:27 +0000

    COMPLETED TASK 4

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,19 @@
-### Part Four -- your code goes here. 
+import random
+
+random_numbers = []
+
+for _ in range(10):
+    random_numbers.append(random.randint(1,100))
+
+for number in random_numbers:
+    print(number, end = ', ')
+
+i = 0
+while i < len(random_numbers):
+    if random_numbers[i] % 2 == 0:
+        random_numbers.pop(i)
+    else:
+        i += 1
+
+
+print("\nThe remaining odd numbers are: ", random_numbers)
