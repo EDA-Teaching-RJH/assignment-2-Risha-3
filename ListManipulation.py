dd73464e9b79256c91229d377c2256166b42e26e
Risha-3 <rs989@kent.ac.uk>
2024-10-24 20:52:49 +0000

    COMPLETED TASK 3

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,1 +1,10 @@
-### Part Three -- your code goes here. 
+myList = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+myList.sort()
+
+while 1 in myList:
+    myList.remove(1)
+
+myList.extend([7,8])
+
+print(myList)
